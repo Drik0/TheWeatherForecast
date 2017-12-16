e7a811b2e50083546b7fae653c2cf9b1d17cfb20
Gerson <gerson.lcc@hotmail.com>
2017-12-16 19:49:26 +0000

    Download weather message

--- TheWeatherForecast/ViewController.swift
+++ TheWeatherForecast/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController/*, UITextFieldDelegate*/ {
 
     @IBOutlet weak var locationTxtField: UITextField!
     
@@ -17,15 +17,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationTxtField.delegate = self
+        //locationTxtField.delegate = self
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest") {
+        
+            let request = NSMutableURLRequest(url: url)
+            var webMessage = ""
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+                if let error = error {
+                    print(error)
+                } else {
+                    if let unwrappedData = data {
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                            if contentArray.count > 0 {
+                             stringSeparator = "</span>"
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                                if newContentArray.count > 0 {
+                                    webMessage = newContentArray[0]
+                                    print(webMessage)
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+
     }
 
     @IBAction func getWeatherBtn(_ sender: UIButton) {
     }
-    
+    /*
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    */
 }
 
